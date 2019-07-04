31ec5a971fb9a105ee2aee71837b76c6a8912e05
E.B <edenbenbow@yahoo.com>
2019-07-04 02:31:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,4 +1,10 @@
 function basicTeenager(age){
+  if (age)>12 && age<20) {
+    return"You are a teenager!"
+  }
+}
+
+function teenager(age){
   if (age>12 && age<20) {
     return"You are a teenager!"
 
