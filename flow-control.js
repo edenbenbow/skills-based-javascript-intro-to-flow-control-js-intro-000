ef582919bf89ced8957e9e0fbf0b876d1b000693
E.B <edenbenbow@yahoo.com>
2019-07-04 02:15:10 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,23 @@
-function basicTeenager(age) {
+function teenager(age){
+  if (age>12 && age<20) {
+    return"You are a teenager!"
+
+  } else {
+    return"You are not a teenager"
+  }
 
 }
 
-function teenager(age) {
 
+function ageChecker(age){
+  if (age>12 && age<20) {
+    return"You are a teenager!"
+} else if (age<13) {
+  return"You are a kid"
 }
 
-function ageChecker(age) {
-
-}
-
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
+else {
+    return"You are a grownup"
+  }
 
 }
